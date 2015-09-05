b180a2ddefac713daf4f726ef8ab518603881e3a
Aditya Bhardwaj <aditya2204@gmail.com>
2015-09-05 14:26:54 +0530

    token fix
    token fix
    
    fix logic
    
    minor change

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,14 +4,13 @@ class SearchController < ApplicationController
   def index
     if params[:search]
       date_count = {new: 0, mid: 0, old: 0}
-      issue_dates = @client.list_issues(params[:search]).select{ |x| !x[:pull_request] }
-                    .map{ |x| x[:created_at].in_time_zone('Chennai').to_date }
-      issue_dates.map{ |x|
-        date_diff = date_diff x
-        case date_diff
-        when 0
+      issue_times = @client.list_issues(params[:search]).select{ |x| !x[:pull_request] }
+                    .map{ |x| x[:created_at].in_time_zone('Chennai') }
+      issue_times.map{ |x|
+        case x
+        when 24.hours.ago < x
           date_count[:new] += 1
-        when 1..6
+        when 7.days.ago < x
           date_count[:mid] += 1
         else
           date_count[:old] += 1
@@ -24,12 +23,6 @@ class SearchController < ApplicationController
   private
 
   def init_octokit
-    @client ||= Octokit::Client.new(access_token: "14aa014d7f548986d00b50298cf1312b7aa41ca7")
     Octokit.auto_paginate = true
-  end
-
-  def date_diff(date)
-    today = Date.today
-    diff = (today - date).round
   end
 end
